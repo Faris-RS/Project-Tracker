9691f52620e0872c46cf2bc478555a56d9c032cf
Faris-RS <brotofarisrs@gmail.com>
2023-07-04 17:03:40 +0530

    First commit

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Project starts here</Text>
+      <Text>Faris was here</Text>
       <StatusBar style="auto" />
     </View>
   );

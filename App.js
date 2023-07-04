03d2e7bde8b82e201e43e8bc58b33202b4d389bf
Ameen-vv <mohammedameenvv@gmail.com>
2023-07-04 17:01:29 +0530

    first commit

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Project starts here</Text>
+      <Text>starts here</Text>
       <StatusBar style="auto" />
     </View>
   );
